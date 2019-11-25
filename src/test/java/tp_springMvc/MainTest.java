e4df8748ab960dc1f92d04dfc01a3d9c97217bf2
souhail el fahimi <sefsef12364@gmail.com>
2019-11-25 23:27:34 +0100

    test trigger

---
+++ src/test/java/tp_springMvc/MainTest.java
@@ -0,0 +1,18 @@
+package tp_springMvc;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+public class MainTest  {
+
+	@Test
+	public void test()
+	{
+		//fail("madarna walo");
+		assertEquals(0, 0);
+	}
+	
+	
+}
