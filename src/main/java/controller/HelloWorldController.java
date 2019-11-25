a0704aa5af5428de6a240a245dc0e62cd56f2009
souhail el fahimi <sefsef12364@gmail.com>
2019-11-25 21:39:24 +0100

    first commit

---
+++ src/main/java/controller/HelloWorldController.java
@@ -0,0 +1,19 @@
+package controller;
+
+import Model.HelloWorld;
+import java.time.LocalDateTime;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+    @RequestMapping("/helloworld")
+    public String handler(Model model) {
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.setMessage("Hello World Example Using Spring MVC 5!!!");
+        helloWorld.setDateTime(LocalDateTime.now().toString());
+        model.addAttribute("helloWorld", helloWorld);
+        return "helloworld";
+    }
+}
